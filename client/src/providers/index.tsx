b55f1f00711feb31e7dc22a902765bca362fda3d
Marlon <martinez.1ded@gmail.com>
2021-01-19 20:00:16 -0600

    material ui support

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from "@material-ui/core";
 import React, { Fragment } from "react";
 import { Router } from "../routes";
 import { AppProvider } from "./App";
 import { AuthProvider } from "./Auth";
+import { theme } from "./Theme";
 
 export const Providers: React.FC = () => {
     return (
         <Fragment>
             <AppProvider>
                 <AuthProvider>
-                    <Router />
+                    <ThemeProvider theme={theme}>
+                        <Router />
+                    </ThemeProvider>
                 </AuthProvider>
             </AppProvider>
         </Fragment>
